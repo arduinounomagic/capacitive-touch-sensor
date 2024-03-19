b323b5d347987871eb43b89659316d294bb7bcab
arduinounomagic <arduinounomagic@gmail.com>
2024-03-19 16:27:12 +0530

    Update capacitive_touch_sensor.ino

--- capacitive_touch_sensor.ino
+++ capacitive_touch_sensor.ino
@@ -1,18 +1,18 @@
-const int Touch_pin=2;
+const int Touch_pin=2; //Connect the sensor at pin 2
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(Touch_pin, INPUT);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(Touch_pin, INPUT); //Set sensor pin as input pin
+  pinMode(LED_BUILTIN, OUTPUT); //Set builtin LED pin as output pin
   
 }
 
 void loop() {
-int state=digitalRead(Touch_pin);
-digitalWrite(LED_BUILTIN,state);
-Serial.print("state of builtin LED is:");
-Serial.println(state);
-delay(1000);
+int state=digitalRead(Touch_pin); //Read the input at touch sensor
+digitalWrite(LED_BUILTIN,state); //Operate the LED according to the input
+Serial.print("State of builtin LED is:");//Print the state on LED, 1 as ON OR 0 as OFF
+Serial.println(state);//Print 0 OR 1 state of LED
+delay(1000);//Wait for 1 second
 
 }
